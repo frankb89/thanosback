a4cfef37de2aed2fee0dbc615f14566a9ce0b7dc
Frank Blanco <frankito89@gmail.com>
2022-03-30 18:06:41 +0200

    secret key in .env file

--- thanosback/settings.py
+++ thanosback/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*a+6o9sk7naua=%!*am!^=w_)^_u5syx&%vw8#y!&13%+dd@3r'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
